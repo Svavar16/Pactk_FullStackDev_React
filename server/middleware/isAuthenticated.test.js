378be51385fa0be1da33d9c7775bf37e56c3e68e
svavar16 <spg009@gmail.com>
2019-10-24 16:21:28 +0000

    adding jest test

---
+++ server/middleware/isAuthenticated.test.js
@@ -0,0 +1,18 @@
+import isAuthenticated from "./isAuthenticated";
+import logger from "./logger";
+
+test('It calls next()', () => {
+    const next = jest.fn(x => x);
+    logger({},{}, next);
+    console.log(next.mock.calls);
+    expect(next.mock.calls.length).toBe(1);
+});
+
+test('User is authenticated', () => {
+    const next = jest.fn(x => x);
+    const req = {};
+    isAuthenticated(req, {}, next);
+    expect(next.mock.calls.length).toBe(1);
+    expect(typeof req.isAuthenticated === "boolean").toBe(true);
+    expect(typeof req.isAdmin === "boolean").toBe(true);
+});
---
+++ server/middleware/logger.test.js
@@ -0,0 +1,8 @@
+import logger from "./logger";
+
+test('It calls next()', () => {
+    const next = jest.fn(x => x);
+    logger({},{}, next);
+    console.log(next.mock.calls);
+    expect(next.mock.calls.length).toBe(1);
+});
