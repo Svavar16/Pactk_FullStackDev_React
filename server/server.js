3086fea7f727c67a1c1f9a3644c9ee6d55667760
svavar16 <spg009@gmail.com>
2019-11-15 13:08:58 +0000

    finished with Crud operations

--- server/server.js
+++ server/server.js
@@ -11,8 +11,8 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(logger);
 app.use(isAuthenticated);
+app.use(logger);
 
 app.get('/v1/users', async (req, res) => {
     const users = await userModel.find() || [];
@@ -33,6 +33,7 @@ app.get('/v1/users/:id', async (req, res) => {
 });
 
 app.post('/v1/users', (req, res) => {
+    // TODO: implement
     res.status(200).end();
 });
 
@@ -61,8 +62,44 @@ app.get('/v1/products/:id', async (req, res) => {
         res.status(404).end();
     }
 
+});
+
+app.post('/v1/products', async (req, res) => {
+    const product = await productModel.create(req.body);
+    if(product){
+        res.status(200).end();
+    } else {
+        res.status(500).end();
+    }
+});
+
+app.put('/v1/products/:id', async (req, res) => {
+    productModel.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        (err) => {
+            if(err){
+                res.status(500).end();
+            } else {
+                res.status(200).end();
+            }
+        }
+    );
+});
+
+app.delete('/v1/products/:id', (req, res) =>{
+    productModel.findByIdAndDelete(
+        req.params.id,
+        (err) => {
+            if(err){
+                res.status(500).end();
+            } else {
+                res.status(200).end();
+            }
+        }
+    )
 })
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+    console.log(`> Server is listening on port ${PORT}`);
 });
