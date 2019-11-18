ea92bee7a8f489f034938a152b13862caf71099f
svavar16 <spg009@gmail.com>
2019-11-18 12:27:13 +0000

    added the react, did the first component

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import DynamicCounter from './components/DynamicCounter';
+
+function App() {
+  return (
+    <div className="App-header">
+      <DynamicCounter />
+    </div>
+  );
+}
+
+export default App;
